803cd84ec8975ad09e7908e73205dabcac1e08aa
alek <wait82985@gmail.com>
2023-09-03 17:24:14 +0600

    problem solved. Top-down DP

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int numberOfWays(int startPos, int endPos, int k) {
-        return  0;
+        Map<String, Integer> memo = new HashMap<>();
+        return  dfs(startPos, endPos, k, memo);
+    }
+    private int dfs(int startPos, int endPos, int k, Map<String, Integer> memo){
+        int modulo = 1_000_000_007;
+        if(k == 0 && startPos == endPos){
+            return 1;
+        }
+        if(k == 0){
+            return 0;
+        }
+        if(memo.containsKey(startPos + " " + k)){
+            return memo.get(startPos + " " + k);
+        }
+        int result = 0;
+        int moveLeft = 0;
+        int moveRight = 0;
+        if(endPos - startPos < k || startPos > endPos) {
+            moveLeft = dfs(startPos - 1, endPos, k - 1, memo) % modulo;
+        }
+        if(endPos > startPos || startPos - endPos < k) {
+            moveRight = dfs(startPos + 1, endPos, k - 1, memo) % modulo;
+        }
+        result = (moveLeft + moveRight) % modulo;
+        memo.put(startPos + " " + k, result);
+        return  result;
     }
 }
